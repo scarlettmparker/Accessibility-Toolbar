f40afc23f8a27722c8508cd9c5aecd803b3c60e3
scarlettp1619 <95775118+scarlettp1619@users.noreply.github.com>
2024-01-26 15:14:00 +0000

    some pep8 garbo

--- accessibility/cifar10.py
+++ accessibility/cifar10.py
@@ -5,10 +5,10 @@ import tensorflow as tf
 from keras import regularizers
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
-from keras.callbacks import LearningRateScheduler
 from keras.callbacks import EarlyStopping
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Activation, Conv2D, MaxPooling2D, BatchNormalization, Dropout
+from keras.layers import Dense, Flatten, Activation, Conv2D
+from keras.layers import MaxPooling2D, BatchNormalization, Dropout
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 from datasets import load_dataset
@@ -18,89 +18,110 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 batch_size = 32
 IMG_SIZE = 32
 
-dataset = load_dataset("parquet", data_files={'train': 'datasets/cifar10/plain_text/train-00000-of-00001.parquet',
-                                              'test': 'datasets/cifar10/plain_text/test-00000-of-00001.parquet'})
+dataset_loc = 'datasets/cifar10/plain_text/'
+dataset = load_dataset("parquet", data_files=
+                       {'train': dataset_loc + 'train-00000-of-00001.parquet',
+                        'test': dataset_loc + '/test-00000-of-00001.parquet'})
 
-train_images = np.array(dataset['train']['img'])
+train_images = np.array(dataset['train']['img']) # converted for training
 test_images = np.array(dataset['test']['img'])
 
 train_images = train_images.reshape(-1, IMG_SIZE, IMG_SIZE, 3) / 255.0
 test_images = test_images.reshape(-1, IMG_SIZE, IMG_SIZE, 3) / 255.0
 
-label_encoder = LabelEncoder()
+label_encoder = LabelEncoder() # used for one hot encoding
 train_captions_encoded = label_encoder.fit_transform(dataset['train']['label'])
 test_captions_encoded = label_encoder.transform(dataset['test']['label'])
 
 train_captions_categorical = to_categorical(train_captions_encoded)
 test_captions_categorical = to_categorical(test_captions_encoded)
 
-weight_decay = 1e-4
+weight_decay = 1e-4 # used to reduce overfitting
 model = Sequential()
-model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
-model.add(Activation('elu'))
-model.add(BatchNormalization())
-model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-model.add(Activation('elu'))
+model.add(Conv2D(32, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay),
+                 input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+model.add(Activation('relu'))
+model.add(BatchNormalization()) # more overfitting prevention
+model.add(Conv2D(32, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.2))
  
-model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-model.add(Activation('elu'))
+model.add(Conv2D(64, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-model.add(Activation('elu'))
+model.add(Conv2D(64, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.3))
  
-model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-model.add(Activation('elu'))
+model.add(Conv2D(128, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-model.add(Activation('elu'))
+model.add(Conv2D(128, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.5))
 
 model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(10, activation='softmax')) # matches number of classes in set
 
-opt_rms = keras.optimizers.RMSprop(lr=0.001)
-model.compile(optimizer=opt_rms, loss='categorical_crossentropy', metrics=['accuracy'])
+opt = keras.optimizers.Adam(lr=0.001)
+model.compile(optimizer=opt,
+              loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.summary()
 
-datagen = ImageDataGenerator(featurewise_center=False,
-        samplewise_center=False,
-        featurewise_std_normalization=False,
-        samplewise_std_normalization=False,
-        zca_whitening=False,
-        rotation_range=20,
-        zoom_range = 0,
-        shear_range=0,
-        width_shift_range=0,
-        height_shift_range=0,
-        horizontal_flip=True,
-        vertical_flip=False,
-        validation_split=0.2)
+# used to augment (modify) the images to artificially increase dataset
+datagen = ImageDataGenerator(featurewise_center=False, 
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    rotation_range=20,
+    zoom_range = 0,
+    shear_range=0,
+    width_shift_range=0,
+    height_shift_range=0,
+    horizontal_flip=True,
+    vertical_flip=False,
+    validation_split=0.2
+)
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-filepath="saved_models/1 - cifar10-100/weights-improvement-{epoch:02d}-{val_accuracy:.2f}.keras" # includes epoch and validation accuracy
-checkpoint = ModelCheckpoint(filepath, verbose=1, save_best_only=True, mode='min')
-reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=4, min_lr=1e-6)
+# callbacks to prevent data loss and training when unnecessary
+early_stopping = EarlyStopping(monitor='val_loss',
+                               patience=10,
+                               restore_best_weights=True)
+filepath = "saved_models/1 - cifar10-100/weights-improvement-"
++ "{epoch:02d}-{val_accuracy:.2f}.keras"
+checkpoint = ModelCheckpoint(filepath, verbose=1,
+                             save_best_only=True,
+                             mode='min')
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss',
+                                                 factor=0.5,
+                                                 patience=4,
+                                                 min_lr=1e-6)
 
 callbacks_list = [checkpoint, reduce_lr]
 
-train_batches = len(train_images) // batch_size
-validation_batches = len(test_images) // batch_size
-
 history = model.fit(
-    datagen.flow(train_images, train_captions_categorical, batch_size=batch_size),
+    datagen.flow(train_images,
+                 train_captions_categorical,
+                 batch_size=batch_size),
     batch_size=batch_size,
     epochs=135,
     verbose=1,
-    validation_data=(test_images, test_captions_categorical),
+    validation_data=(test_images,
+                     test_captions_categorical),
     shuffle=True,
     callbacks=callbacks_list
 )
--- accessibility/cifar100.py
+++ accessibility/cifar100.py
@@ -6,22 +6,25 @@ import logging
 from tensorflow import keras
 from keras import regularizers
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Activation, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Input
+from keras.layers import Dense, Flatten, Activation, MaxPooling2D
+from keras.layers import BatchNormalization, Dropout, Conv2D
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 from datasets import load_dataset
 
-logging.getLogger('tensorflow').disabled = True
+logging.getLogger('tensorflow').disabled = True # ignore warnings
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 batch_size = 64
 IMG_SIZE = 32
 
-dataset = load_dataset("parquet", data_files={'train': 'datasets/cifar100/cifar100/train-00000-of-00001.parquet',
-                                              'test': 'datasets/cifar100/cifar100/test-00000-of-00001.parquet'})
+dataset_loc = 'datasets/cifar100/cifar100/'
+dataset = load_dataset("parquet", data_files=
+                       {'train': dataset_loc + 'train-00000-of-00001.parquet',
+                        'test': dataset_loc + '/test-00000-of-00001.parquet'})
 
 train_images = np.array(dataset['train']['img'])
 test_images = np.array(dataset['test']['img'])
@@ -29,60 +32,61 @@ test_images = np.array(dataset['test']['img'])
 train_images = train_images.reshape(-1, IMG_SIZE, IMG_SIZE, 3) / 255.0
 test_images = test_images.reshape(-1, IMG_SIZE, IMG_SIZE, 3) / 255.0
 
-train_captions_fine = dataset['train']['fine_label']
-test_captions_fine = dataset['test']['fine_label']
+train_captions = dataset['train']['fine_label']
+test_captions = dataset['test']['fine_label']
 
-train_captions_fine = [x + 10 for x in train_captions_fine]
-test_captions_fine = [x + 10 for x in test_captions_fine]
+train_captions = [x + 10 for x in train_captions]
+test_captions = [x + 10 for x in test_captions]
 
 label_encoder_fine = LabelEncoder()
-train_captions_fine_encoded = label_encoder_fine.fit_transform(train_captions_fine)
-test_captions_fine_encoded = label_encoder_fine.transform(test_captions_fine)
+train_captions_encoded = label_encoder_fine.fit_transform(train_captions)
+test_captions_encoded = label_encoder_fine.transform(test_captions)
 
-train_captions_fine_categorical = to_categorical(train_captions_fine_encoded, 100)
-test_captions_fine_categorical = to_categorical(test_captions_fine_encoded, 100)
+train_captions_categorical = to_categorical(train_captions_encoded, 100)
+test_captions_categorical = to_categorical(test_captions_encoded, 100)
 
 weight_decay = 1e-4
 model = Sequential()
-model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+model.add(Conv2D(32, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay),
+                 input_shape=(IMG_SIZE, IMG_SIZE, 3)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
-model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Conv2D(32, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.2))
  
-model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Conv2D(64, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
-model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Conv2D(64, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.3))
  
-model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Conv2D(128, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
-model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Conv2D(128, (3,3), padding='same',
+                 kernel_regularizer=regularizers.l2(weight_decay)))
 model.add(Activation('elu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.5))
 model.add(Flatten())
 
-#model.add(Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#bias_regularizer=keras.regularizers.l2(1e-4), activity_regularizer=keras.regularizers.l2(1e-5)))
-#model.add(BatchNormalization())
-#model.add(Dropout(0.25))
-#model.add(Dense(128,activation="relu",kernel_regularizer=keras.regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#bias_regularizer=keras.regularizers.l2(1e-4), activity_regularizer=keras.regularizers.l2(1e-5)))
-#model.add(BatchNormalization())
-
 model.add(Dense(100, activation='softmax'))
 opt = keras.optimizers.Adam(lr=0.001)
-model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'], loss_weights=[1.0, 1.0])
+model.compile(optimizer=opt, loss='categorical_crossentropy',
+              metrics=['accuracy'],
+              loss_weights=[1.0, 1.0])
 
 model.summary()
 
@@ -100,12 +104,23 @@ datagen = ImageDataGenerator(featurewise_center=False,
         vertical_flip=False,
         validation_split=0.2)
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-filepath="saved_models/2 - cifar100/weights-improvement-{epoch:02d}-{val_accuracy:.2f}.keras" # includes epoch and validation accuracy
-checkpoint = ModelCheckpoint(filepath, verbose=1, save_best_only=True, mode='min')
-logdir = os.path.join("saved_models/2 - cifar100/logs_cifar_100", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
-reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=4, min_lr=1e-6)
+early_stopping = EarlyStopping(monitor='val_loss',
+                               patience=10,
+                               restore_best_weights=True)
+filepath="saved_models/2 - cifar100/weights-improvement-"
++ "{epoch:02d}-{val_accuracy:.2f}.keras" # includes epoch and validation acc.
+checkpoint = ModelCheckpoint(filepath,
+                             verbose=1,
+                             save_best_only=True,
+                             mode='min')
+logdir = os.path.join("saved_models/2 - cifar100/logs_cifar_100",
+                      datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir,
+                                                      histogram_freq=1)
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss',
+                                                 factor=0.5,
+                                                 patience=4,
+                                                 min_lr=1e-6)
 
 callbacks_list = [checkpoint, reduce_lr, tensorboard_callback]
 
@@ -113,11 +128,14 @@ train_batches = len(train_images) // batch_size
 validation_batches = len(test_images) // batch_size
 
 history = model.fit(
-    datagen.flow(train_images, train_captions_fine_categorical, batch_size=batch_size),
+    datagen.flow(train_images,
+                 train_captions_categorical,
+                 batch_size=batch_size),
     batch_size=batch_size,
     epochs=350,
     verbose=1,
-    validation_data=(test_images, test_captions_fine_categorical),
+    validation_data=(test_images,
+                     test_captions_categorical),
     shuffle=True,
     callbacks=callbacks_list
 )
