afa7d7f92fce328c8183d118b73c5551c90d4186
scarlettp1619 <95775118+scarlettp1619@users.noreply.github.com>
2024-02-20 12:26:06 +0000

    renamed some folders and some stuff
    this is to not confuse people if they want to look at the source code (chrome-extension) as the folder was previously called "extension"

--- accessibility/apps.py
+++ accessibility/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccessibilityConfig(AppConfig):
+class DjangoProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accessibility'
+    name = 'django-project'
